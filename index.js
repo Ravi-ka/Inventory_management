c426a270e8321d96ca707a670fceef587a87261a
Ravisankar Ka <ka.ravisankar@gmail.com>
2023-09-21 14:02:21 +0530

    Implemented sessions, cookies

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 import validationMiddleware from './src/middlewares/validation.middleware.js';
 import { uploadFile } from './src/middlewares/file-upload.middleware.js';
+import UserController from './src/controllers/user.controller.js';
+import session from 'express-session';
+import {auth} from './src/middlewares/auth.middleware.js';
+import cookieParser from 'cookie-parser';
+import { setLastVisit } from './src/middlewares/lastVisit.middleware.js';
 
 const app = express();
 app.use(express.static('public'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use(cookieParser());
+//app.use(setLastVisit)
 
 const productsController =
   new ProductsController();
+  const userController = new UserController();
 
 app.use(ejsLayouts);
 app.use(express.json());
@@ -20,24 +34,29 @@ app.set(
   path.join(path.resolve(), 'src', 'views')
 );
 
-app.get('/', productsController.getProducts);
+app.get('/register',userController.getRegister)
+app.get('/login',userController.getLogin)
+app.post('/register',userController.postRegister)
+app.post('/login',userController.postLogin)
+app.get('/logout',userController.logout)
+app.get('/', setLastVisit,auth,productsController.getProducts);
 app.get(
-  '/add-product',uploadFile.single('imageUrl'),
+  '/add-product',auth,uploadFile.single('imageUrl'),
   productsController.getAddProduct
 );
 app.get(
-  '/update-product/:id',
+  '/update-product/:id',auth,
   productsController.getUpdateProductView
 );
 
-app.post('/delete-product/:id',productsController.deleteProduct)
+app.post('/delete-product/:id',auth,productsController.deleteProduct)
 
 app.post(
-  '/',uploadFile.single('imageUrl'),
+  '/',auth,uploadFile.single('imageUrl'),
   validationMiddleware,
   productsController.postAddProduct
 );
-app.post('/update-product',productsController.postUpdateProduct)
+app.post('/update-product',auth,productsController.postUpdateProduct)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,12 +3,12 @@ import ProductModel from '../models/product.model.js';
 class ProductsController {
   getProducts(req, res, next) {
     var products = ProductModel.getAll();
-    res.render('index', { products });
+    res.render('index', { products,userEmail:req.session.userEmail });
   }
 
   getAddProduct(req, res, next) {
     res.render('new-product', {
-      errorMessage: null,
+      errorMessage: null,userEmail:req.session.userEmail 
     });
   }
 
@@ -17,7 +17,7 @@ class ProductsController {
     const imageUrl = 'images/'+ req.file.filename;
     ProductModel.add(name, desc, price, imageUrl);
     var products = ProductModel.getAll();
-    res.render('index', { products });
+    res.render('index', { products,userEmail:req.session.userEmail  });
   }
 
   getUpdateProductView(req, res, next) {
@@ -27,7 +27,7 @@ class ProductsController {
     if (productFound) {
       res.render('update-product', {
         product: productFound,
-        errorMessage: null,
+        errorMessage: null,userEmail:req.session.userEmail 
       });
     }
     // 2. else return errors.
@@ -39,7 +39,7 @@ class ProductsController {
   postUpdateProduct(req, res,next){
     ProductModel.update(req.body);
     var products = ProductModel.getAll();
-    res.render('index', { products });
+    res.render('index', { products,userEmail:req.session.userEmail  });
   }
 
   deleteProduct(req,res){
@@ -51,7 +51,7 @@ class ProductsController {
     }
     ProductModel.delete(id)
     var products = ProductModel.getAll();
-    res.render('index', { products });
+    res.render('index', { products,userEmail:req.session.userEmail  });
   }
 }
 
